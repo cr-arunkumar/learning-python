65a290c466018fb60dabc408f574a01a682260a2
cs-arunp <arun.kumar@creolestudios.com>
2025-01-24 12:09:42 +0530

    list operations

--- fundamentals/list.py
+++ fundamentals/list.py
@@ -107,3 +107,194 @@ numbers.sort()
 strings.sort()
 sorted_list = numbers + strings
 print("Sorted after separating types:", sorted_list)
+
+for i in range(1,5):
+    print(i)
+
+c=[1,2,3,4]
+for item in c:
+    print(item*55)
+d=[item for item in c]
+print("Dictionary",d)
+import time 
+
+
+result=[item for i in range(10)]
+print(result)
+result=[item for item in range(100001)]
+start_time = time.time()
+print(10000 in result)
+end_time = time.time()
+print("Time taken to check 5 in list: ", end_time - start_time)
+
+result=set(item for item in range(100001))
+start_time = time.time()
+print(10000 in result)
+end_time = time.time()
+print("Time taken to check 5 in set: ", end_time - start_time)
+
+import sys
+# ram in gb 
+print("Python version", sys.version)
+
+# extend 
+list1=[1,2,3,4]
+list2=["a","b","c"]
+
+list1.extend(list2)
+print("Extended list1:",list1)
+
+# insert
+list1.insert(0,"z")
+# insert at the end
+list1.insert(len(list1),"y")
+print("Inserted list1:",list1)
+
+# remove
+list1.append("z")
+print("Removing list1:",list1)
+list1.remove("z")
+print("Removed 'z' from list1:",list1)
+
+
+# count
+print("Count of 'a' in list1:",list1.count("a"))
+print("Len of 'a' in list1:",len(list1))
+
+
+# reverse
+list1.reverse()
+print("Reversed list1:",list1)
+
+# sort
+list1=[str(item) for i in list1]
+list1.sort()
+print("Sorted list1:",list1)
+
+# index
+
+try:
+    print("Index of 'b' in list1:", list1.index("b"))
+except Exception as e:
+    print("Error:", str(e))
+
+
+# join
+print("Joined list1:", ",".join(list1))     
+
+# slicing
+print("Sliced list1:", list1[0:3])
+
+# list comprehension
+print("List Comprehension:", [i for i in range(10) if i%2==0])
+
+# removing the items from the list 
+list1=[1,2,3,4,5]
+del list1[0]
+print("List after removing first item:", list1)
+
+list1.pop(0)
+print("List after popping first item:", list1)
+
+list1.remove(5)
+print("List after removing 5:", list1)
+# pop(index) - it will rm the item based on index
+# remove(value) - it will remove the item based on value else it will throw ValueError
+# clear() - it will clear the list or rm all items from the list and list will have empty 
+# copy() - it will return the shallow copy of the list
+# count(value) - it will return the count of the value in the list
+# extend(iterable) - it will extend the list with the iterable items
+
+print("List before the cleaning up:", list1)
+list1.clear()
+print("List after the cleaning up:", list1)
+list1=[1,2,3,4,5]
+clist1=list1.copy();
+print("List after copying:", clist1)
+# is they the same in terms of memory
+print("Is list1 and clist1 the same?", list1 == clist1)
+print("Is list2 and clist2 the same?", clist1 is list1)
+clist1.append("Hello World")
+print("List after appending 'Hello World' to clist1:", clist1,"list1\n",list1)
+
+
+# looping over the list
+# 1. 
+for i in list1:
+    print(i,end=" ")
+# 2
+for i in range(len(list1)):
+    print(list1[i],end=" ")
+
+# 3
+list_len=len(list1)
+i=list_len-1
+print()
+while i>=0 :
+    print(list1[list_len-i-1],end=" ")
+    i-=1
+
+print("\n----------------------------------------------------------------")
+# 4
+[print(item,end=" ") for item in list1]
+(print(item,end=" ") for item in list1)
+
+
+
+fruits = ['apple', 'banana', 'cherry']
+for index, fruit in enumerate(fruits):
+    print(f'I am {index+1} and I love {fruits[index]}')
+
+# sorting the list 
+list1=[2,33,4,5,6,7,8,9,1]
+list1.sort()
+print("Sorted list:",list1)
+# in reverse order or in decreasing order
+list1.sort(reverse=True)
+print("Reversed list:",list1)
+
+# sorting the list based on length of the strings
+list1=['apple', 'banana', 'cherry', 'date', 'elderberry']
+def MyStringFunc(e):
+      print("🚀 ~ e:", e)
+      return len(e)
+
+list1.sort(key=MyStringFunc)
+print("List sorted by length:",list1)
+
+list1=['apple', 'Banana', 'cherry', 'Date', 'elderberry']
+list1.sort(key=lambda x : x.lower())
+print("List sorted",list1)
+
+# copying the list
+list1=['apple', 'Banana', 'cherry', 'Date', 'elderberry']
+l1Copy=list1.copy()
+l2Copy=list(list1);
+l3Copy=list1
+l4Copy=list[:];
+
+print("Copied list:",l1Copy)
+print("Is list1 and l3Copy sorted the same?",list1 is l3Copy)
+print("Is list1 and l4Copy sorted the same?",list1 is l4Copy)
+print("Is list1 and l2Copy sorted the same?",list1 is l2Copy)
+print("Is list1 and l1Copy the same?",list1 is l1Copy)
+
+
+#  Join Lists
+list1=['apple', 'banana', 'cherry']
+list2=['date', 'elderberry']
+
+list1.extend(list2)
+print("Extended list:",list1)
+
+list1=['apple', 'banana', 'cherry']
+list2=['date', 'elderberry']
+list1=list1+list2
+print("Concatenated list:",list1)
+
+list1=[1,2,3]
+list2=[4,5,6]
+
+for i in list2:
+    list1.append(i)
+print("Appended list:",list1)
